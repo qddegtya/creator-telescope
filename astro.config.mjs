a7052d47440043178293f12b9a1c1ac90bed7439
archer <qddegtya@gmail.com>
2024-12-23 13:12:52 +0800

    feat: optimize redirect config

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,5 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   output: "static",
-  site: "https://ct-weekly.xiaoa.name",
-  redirects: {
-    '/newsletter': '/newsletter/1'
-  }
+  site: "https://ct-weekly.xiaoa.name"
 });
